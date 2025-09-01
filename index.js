082d4c0e7482f997421c7fcc6dce4d468eb7e16b
sonaa1107 <sonaagrawal1107@gmail.com>
2025-09-01 11:22:23 +0530

    add connection file for mongo db

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-const express=require("express")
+const express=require("express");
+const { connectToMongo } = require("./connect");
+
 const app=express();
 const PORT=80;
-const mongoose=require("mongoose");
-
-
-
+//connection
+connectToMongo("mongodb://127.0.0.1:27017/contact-form");
+//middlewares
 app.use(express.json())
 app.use(express.static("public"))
 app.use(express.urlencoded({extended:true}))
+
 app.get("/",(req,res)=>{
     res.send("hello world");
 })
